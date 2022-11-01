47151baeafa44fc0c1a67bbad1fe046cb30bc524
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-01 06:30:39 +0000

    style: format

--- __tests__/polyfill.ts
+++ __tests__/polyfill.ts
@@ -2,4 +2,4 @@ import { TextEncoder } from 'util'
 import { Crypto } from '@peculiar/webcrypto'
 
 globalThis.crypto = new Crypto() as globalThis.Crypto
-globalThis.TextEncoder = TextEncoder 
+globalThis.TextEncoder = TextEncoder
